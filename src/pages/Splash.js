db4c87be214982aee59263181279a74650678ed6
Siger Young <yscysc01@qq.com>
2016-03-07 21:57:15 +0800

    Add splash

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,17 +1,20 @@
 import React, {
   View,
+  Image,
   Text,
 } from 'react-native';
 
 import styles from './Splash.styles';
 
 export default class Splash extends React.Component {
+  splash = require('../assets/splash.png');
   render() {
+    // <Text style={styles.title}>
+    //   Teeworlds 中国
+    // </Text>
     return (
       <View style={styles.centerContainer}>
-        <Text style={styles.title}>
-          Hello, React-Native Starter Kit!
-        </Text>
+        <Image style={styles.splashImage} source={this.splash} />
       </View>
     );
   }
--- src/pages/Splash.styles.js
+++ src/pages/Splash.styles.js
@@ -1,11 +1,15 @@
 import {
   StyleSheet,
+  Dimensions,
 } from 'react-native';
 
 import globalStyles from '../styles';
+const {height, width} = Dimensions.get('window');
 
 const localStyles = StyleSheet.create({
-  title: {
+  splashImage: {
+    width: width,
+    height: height,
   },
 });
 export default {
