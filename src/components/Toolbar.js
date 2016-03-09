a3dbfea5a4fc56ea90d14aac0a7bfeaa5937f529
siger-young <yscysc01@qq.com>
2016-03-09 23:58:46 +0800

    Change toolbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -8,14 +8,7 @@ import * as icons from '../icons'
 class DrawerNavigation extends React.Component {
   render() {
     return(
-      <ToolbarAndroid
-        title={this.props.title}
-        titleColor={'#FFFFFF'}
-        navIcon={this.props.navIcon}
-        onIconClicked={this.props.onIconClicked}
-        subtitle={this.props.subtitle}
-        subtitleColor={this.props.subtitleColor}
-        style={styles.toolbar} />
+
     )
   }
 }
